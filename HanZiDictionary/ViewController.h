b6937a9ee42723be0dc7c17569d4ad7c50accf3e
wu-mao <wu-mao@qq.com>
2013-06-21 18:49:41 +0800

    Initial Commit

---
+++ HanZiDictionary/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  HanZiDictionary
+//
+//  Created by ibokan on 13-6-21.
+//  Copyright (c) 2013年 ibokan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
